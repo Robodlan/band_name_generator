47ee483de578a3cf8497534e4c17da24d5a88fb5
Robodlan <azuldevs@zohomail.com>
2024-07-18 11:24:01 -0400

    Add style.css

--- index1.js
+++ index1.js
@@ -1,19 +1,16 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-import morgan from "morgan";
-
+// import morgan from "morgan";
+const _dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello Reinaldo</h1>")
+    res.sendFile(_dirname + "/public/index.html")
 });
-
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
