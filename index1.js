9b43f72f34eb5760bd9a7ef13523b889c3781510
Robodlan <azuldevs@zohomail.com>
2024-07-19 08:20:55 -0400

    middleware

--- index1.js
+++ index1.js
@@ -2,16 +2,16 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
-import morgan from "morgan";
+
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
-const bandName = "";
+let bandName = "";
 
 const logger = (req, res, next) => {
-  console.log("request method", req.method);
-  console.log("request url", req.url);
+    console.log(req.body);
+   bandName = req.body["street"] + req.body["pet"]
   next();
 }
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -22,10 +22,9 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
-   console.log(req.body);
    res.send(`
     <h1>Your Band Name is</h1>
-    <h2>${req.body.street}${req.body.pet}</h2>`)
+    <h2>${bandName}</h2>`)
 });
 
 app.listen(port, () => {
